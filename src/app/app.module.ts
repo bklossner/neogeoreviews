84534015d49accec59c8e416d076de6ef12827e7
Bob K <farfisa5@gmail.com>
2018-10-17 23:18:17 -0500

    Importing GameComponent and GameListComponent.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { fas } from '@fortawesome/free-solid-svg-icons';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { GameListComponent } from './game-list/game-list.component';
 
 library.add(fas);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    GameListComponent
   ],
   imports: [
     BrowserModule,
